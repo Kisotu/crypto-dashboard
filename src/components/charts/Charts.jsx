14dce9144455b6b641eff2cd131108470781dc43
Jake Kisotu <55947083+JakeKisotu@users.noreply.github.com>
2022-06-29 16:48:59 +0300

    removed unused import

--- src/components/charts/Charts.jsx
+++ src/components/charts/Charts.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Chart from 'chart.js/auto';
 
 const Charted =()=> {
-    const chart = new Chart()
+    // const chart = new Chart()
     return(
         
         <div>
